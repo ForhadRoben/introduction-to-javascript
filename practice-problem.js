e76ce06348cc9bdb96a964697b2587be8d27c836
ForhadRobe <i.rforhad@gmail.com>
2022-07-31 18:12:18 +0600

    first commit

---
+++ practice-problem.js
@@ -0,0 +1,19 @@
+var total_tk = 1000, orange = 400, apple = 300;
+var orange_and_apple = orange + apple;
+var result = total_tk - orange_and_apple;
+// console.log(result);
+
+let math = 75.25, biology = 65, chemistry = 80, physics = 35.45, bangla = 99.50;
+let total = (math + biology + chemistry + physics + bangla) / 5;
+total = total.toFixed(2);
+result1 = parseFloat(total);
+// console.log(result1);
+
+var first_line = "I am going to be";
+var second_line = "an awesome web devoloper";
+var one_line = first_line + " " + second_line;
+// console.log(one_line);
+const remainder = 119 % 5
+console.log(remainder);
+
+
